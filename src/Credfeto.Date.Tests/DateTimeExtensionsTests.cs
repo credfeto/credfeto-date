9dc39aa1cd7b709777058ef5cf4801ee9edb35b1
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-04-26 13:45:12 +0000

    Cleanup: DateTimeExtensionsTests.cs

--- src/Credfeto.Date.Tests/DateTimeExtensionsTests.cs
+++ src/Credfeto.Date.Tests/DateTimeExtensionsTests.cs
@@ -10,15 +10,7 @@ public sealed class DateTimeExtensionsTests : TestBase
     [Fact]
     public void DateInUtcConvertsToDateTimeOffset()
     {
-        DateTime date = new(
-            year: 2021,
-            month: 1,
-            day: 1,
-            hour: 0,
-            minute: 0,
-            second: 0,
-            kind: DateTimeKind.Utc
-        );
+        DateTime date = new(year: 2021, month: 1, day: 1, hour: 0, minute: 0, second: 0, kind: DateTimeKind.Utc);
 
         DateTimeOffset result = date.AsDateTimeOffset();
 
@@ -31,15 +23,7 @@ public sealed class DateTimeExtensionsTests : TestBase
     [InlineData(DateTimeKind.Unspecified)]
     public void DateNotInUtcDoesNotConvertToDateTimeOffset(DateTimeKind dateTimeKind)
     {
-        DateTime date = new(
-            year: 2021,
-            month: 1,
-            day: 1,
-            hour: 0,
-            minute: 0,
-            second: 0,
-            kind: dateTimeKind
-        );
+        DateTime date = new(year: 2021, month: 1, day: 1, hour: 0, minute: 0, second: 0, kind: dateTimeKind);
 
         Assert.Throws<ArgumentOutOfRangeException>(() => date.AsDateTimeOffset());
     }
