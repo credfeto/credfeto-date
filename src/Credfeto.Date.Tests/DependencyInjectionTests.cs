1fa760770d0bf540b911a9cd6be91063a4da7174
Mark Ridgwell <credfeto@users.noreply.github.com>
2025-09-16 23:17:30 +0100

    Renamed

--- src/Credfeto.Date.Tests/DependencyInjectionTests.cs
+++ src/Credfeto.Date.Tests/DependencyInjectionTests.cs
@@ -8,11 +8,13 @@ namespace Credfeto.Date.Tests;
 public sealed class DependencyInjectionTests : DependencyInjectionTestsBase
 {
     public DependencyInjectionTests(ITestOutputHelper output)
-        : base(output: output, dependencyInjectionRegistration: Configure) { }
-
-    private static IServiceCollection Configure(IServiceCollection arg)
+        : base(output: output, dependencyInjectionRegistration: Configure)
     {
-        return arg.AddDate();
+    }
+
+    private static IServiceCollection Configure(IServiceCollection services)
+    {
+        return services.AddDate();
     }
 
     [Fact]
